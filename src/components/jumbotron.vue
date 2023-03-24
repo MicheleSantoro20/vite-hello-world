a0e5bd1c5cb69f2fdb263b54115d5482e43176d3
Michele Santoro <michelesantoro2000@icloud.com>
2023-03-24 14:40:20 +0100

    Caricato esercizio

---
+++ src/components/jumbotron.vue
@@ -0,0 +1,67 @@
+<script>
+
+</script>
+
+<template>
+    <div class="jumbo-img">
+        <div class="mask">
+            <div class="wrapper">
+            <div class="color-white">
+                <h1 class="mb-3">La mia prima app con Vite!</h1>
+                <h4 class="mb-3">Classe 87</h4>
+                <button class="button">Call To Action</button>
+            </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+
+.jumbo-img {
+    margin: 80px;
+    text-align: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    border-radius: 20px;
+    background-image: url('https://s1.1zoom.me/b7053/613/Japan_Tokyo_Parks_Flowering_trees_Rivers_Bridges_550548_2560x1440.jpg');
+    height: 800px;
+}
+.mask {
+    background-color: rgba(0, 0, 0, 0.6);
+    display: inline-block;
+    margin-top: 250px;
+    padding: 100px 50px;
+}
+
+h1 {
+    margin-bottom: 40px;
+    font-size: 60px;
+}
+
+h4 {
+    font-size: 30px;
+}
+.wrapper {
+
+    height: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+
+.button {
+    margin-top: 30px;
+    padding: 20px;
+    font-size: 25px;
+    color: green;
+    background-color: transparent;
+    border: 1px solid white;
+}
+.color-white {
+    color: white;
+}
+
+</style>
